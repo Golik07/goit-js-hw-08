7f989af872a2b0374a72b6fd307ed4bc76cdd7f0
Golik07 <126725620+Golik07@users.noreply.github.com>
2023-07-12 16:57:28 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,27 +6,21 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const fatherImages = document.querySelector('.gallery');
-const imgMarkUp = createImagesCards(galleryItems);
+const imgMarkUp = onImagesCards(galleryItems);
 
-function createImagesCards(imgs) {
+function onImagesCards(imgs) {
   return imgs
     .map(({ preview, original, description }) => {
       return `
-   <li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+    </li>
     `;
     })
     .join('');
 }
-
 fatherImages.insertAdjacentHTML('afterbegin', imgMarkUp);
 
 fatherImages.addEventListener('click', openImgs);
@@ -34,28 +28,13 @@ fatherImages.addEventListener('click', openImgs);
 function openImgs(event) {
   event.preventDefault();
 
-  const imgValue = event.target.dataset.source;
-  const nodeNameImg = event.target.nodeName;
-  const alt = event.target.alt;
-
-  if (nodeNameImg !== 'IMG') {
+  if (event.target.nodeName !== 'IMG') {
     return;
   }
 
-  const image = basicLightbox.create(
-    `
-      <img src="${imgValue}" alt ="${alt}" width="800" height="600" />
-    `,
-    {
-      onShow: image => window.addEventListener('keydown', closeModal),
-      onClose: image => window.removeEventListener('keydown', closeModal),
-    }
-  );
-  image.show();
-
-  closeModal();
-}
-
-function closeModal() {
-  image.close();
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
 }
