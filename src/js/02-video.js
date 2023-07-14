f2acc45a77a8c48c22ce2dc1df43a1dcb4cb458a
Golik07 <126725620+Golik07@users.noreply.github.com>
2023-07-15 00:26:10 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+valueTimeVideo();
+
+const onPlay = throttle(data => {
+  const valueTime = Math.round(data.seconds);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(valueTime));
+  console.log(valueTime);
+}, 1000);
+
+player.on('timeupdate', onPlay);
+
+function valueTimeVideo() {
+  const alert = 'Нужно ввести значение ';
+  const valueVideo = localStorage.getItem('videoplayer-current-time');
+  if (valueVideo) {
+    player.setCurrentTime(valueVideo);
+  }
+  localStorage.removeItem('videoplayer-current-time');
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const text = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
+
+form.addEventListener('submit', formSubmit);
+
+onTextTextarea();
+
+const formValue = {};
+
+form.addEventListener('input', throttle(formInput, 500));
+
+function formInput(e) {
+  formValue[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formValue));
+}
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  const elements = e.currentTarget.elements;
+
+  const email = elements.email.value;
+  const message = elements.message.value;
+
+  if (email === '' || message === '') {
+    return alert('Нужно все поля заполнить');
+  } else {
+    const value = {
+      email,
+      message,
+    };
+    console.log(value);
+    form.reset();
+    localStorage.removeItem('feedback-form-state');
+  }
+}
+
+function onTextTextarea() {
+  const message = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (message) {
+    text.value = message.email;
+    textarea.value = message.message;
+  }
+}
